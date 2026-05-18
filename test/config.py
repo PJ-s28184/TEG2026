5c343a174080698c026e413399b6d83e96db20bb
CaptainBart01 <abartkiewicz01@gmail.com>
2026-05-18 12:51:00 +0200

    Add test module config

---
+++ test/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+TEST_DIR     = Path(__file__).resolve().parent
+ROOT         = TEST_DIR.parent          # katalog główny projektu (TEG2026/)
+PROMPTS_FILE = TEST_DIR / "test_prompts.txt"
